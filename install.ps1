05b11bf7736e350b254610dbff4c2c4edc157de6
chapter9 <1077096+chapter9@users.noreply.github.com>
2021-09-06 16:06:58 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,6 +2,6 @@ param ($param1)
 $ProgressPreference = "SilentlyContinue"
 Write-host Installing using $param1
 wget -UseBasicParsing "https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "~/Downloads/winget.msixbundle"
-Add-AppxPackage -Path winget.msixbundle
+Add-AppxPackage -Path "~/Downloads/winget.msixbundle"
 wget -UseBasicParsing "https://raw.githubusercontent.com/chapter9/install/main/$param1" -OutFile "~/Downloads/$param1"
 winget import (Resolve-Path ~/Downloads/$param1)
