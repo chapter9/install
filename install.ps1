b9ad65786d4840dccaa6f8203538b8d53b158c02
chapter9 <1077096+chapter9@users.noreply.github.com>
2021-09-06 16:03:25 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -4,4 +4,4 @@ Write-host Installing using $param1
 wget -UseBasicParsing "https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle" -OutFile "~/Downloads/winget.msixbundle"
 Add-AppxPackage -Path winget.msixbundle
 wget -UseBasicParsing "https://raw.githubusercontent.com/chapter9/install/main/$param1" -OutFile "~/Downloads/$param1"
-winget import "~/Downloads/$param1"
+winget import (Resolve-Path ~/Downloads/$param1)
