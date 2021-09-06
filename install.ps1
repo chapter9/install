3c5a8a6da31a487c93dbc43991962eecbee980a8
chapter9 <1077096+chapter9@users.noreply.github.com>
2021-09-06 15:34:03 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,3 @@
 $ProgressPreference = "SilentlyContinue"
-wget -UseBasicParsing https://github.com/microsoft/winget-cli/releases/download/v1.0.11451/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.appxbundle -OutFile winget.appxbundle
-Add-AppxPackage -Path winget.appxbundle
+wget -UseBasicParsing https://github.com/microsoft/winget-cli/releases/download/v1.0.11692/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile winget.msixbundle
+Add-AppxPackage -Path winget.msixbundle
